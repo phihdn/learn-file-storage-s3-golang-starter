520e0be55ef8ef25a0a01a0e3ca2f27c2628fad9
Phi <phihdn@gmail.com>
2025-05-21 03:12:38 -0300

    CH1: File Storage L5: Multipart Uploads

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,62 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	// Parse the multipart form with max 10MB
+	const maxMemory = 10 << 20 // 10MB
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error parsing multipart form", err)
+		return
+	}
 
-	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+	// Get the file from form data
+	file, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error getting thumbnail from form", err)
+		return
+	}
+	defer file.Close()
 
-	// TODO: implement the upload here
+	// Get the content type
+	contentType := fileHeader.Header.Get("Content-Type")
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// Read all the image data
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error reading thumbnail data", err)
+		return
+	}
+
+	// Get video metadata and check ownership
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't get video", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You don't own this video", nil)
+		return
+	}
+
+	// Store the thumbnail in memory
+	videoThumbnails[videoID] = thumbnail{
+		data:      imageData,
+		mediaType: contentType,
+	}
+
+	// Update the video metadata with new thumbnail URL
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	video.ThumbnailURL = &thumbnailURL
+
+	// Save the updated video metadata
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		delete(videoThumbnails, videoID)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+
+	// Respond with the updated video metadata
+	respondWithJSON(w, http.StatusOK, video)
 }
